75ade498d8ef576a5fd3d5174619733eafd1a593
olha-makarchuk <olamacarchuk@gmail.com>
2025-07-27 19:59:02 +0300

    add linear search

---
+++ SearchAlgorithms/SearchAlgorithms/LinearSearch.cs
@@ -0,0 +1,22 @@
+﻿namespace SearchAlgorithms
+{
+    //O(N)
+    public class LinearSearch
+    {
+        public int[] NumArray { get; set; }
+        public int SearchedNumber { get; set; }
+
+        public int Search()
+        {
+            for (int i = 0; i < NumArray.Length; i++)
+            {
+                if (NumArray[i] == SearchedNumber)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ SearchAlgorithms/SearchAlgorithms/Program.cs
@@ -0,0 +1,25 @@
+﻿using SearchAlgorithms;
+
+
+int[] arr = { 34, 64, 25, 12, 22, 11, 90 };
+int numberToFind = 22;
+
+
+BinarySearch binarySearch = new BinarySearch
+{
+    NumArray = arr,
+    SearchedNumber = numberToFind
+};
+
+int iterativeResult = binarySearch.SearchIterative();
+int recursiveResult = binarySearch.SearchRecursive();
+
+
+LinearSearch
+linearSearch = new LinearSearch
+{
+    NumArray = arr,
+    SearchedNumber = numberToFind
+};
+
+
