cc17cc191f94d2a7d554d8a4d84c5e73f5f0be37
olha-makarchuk <olamacarchuk@gmail.com>
2025-07-28 18:30:06 +0300

    clear code

--- SortingAlgorithms/SortingAlgorithms/BubbleSort.cs
+++ SortingAlgorithms/SortingAlgorithms/BubbleSort.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SortingAlgorithms
+﻿namespace SortingAlgorithms
 {
-    class BubbleSort
+    //O(N²)
+    public class BubbleSort
     {
+        public int[] NumArray { get; set; }
+
+        public int[] Sort()
+        {
+            for (int i = 0; i < NumArray.Length - 1; i++)
+            {
+                for (int j = 0; j < NumArray.Length - i - 1; j++)
+                {
+                    if (NumArray[j] > NumArray[j + 1])
+                    {
+                        int temp_j = NumArray[j];
+                        NumArray[j] = NumArray[j + 1];
+                        NumArray[j + 1] = temp_j;
+                    }
+                }
+            }
+
+            return NumArray;
+        }
     }
 }
