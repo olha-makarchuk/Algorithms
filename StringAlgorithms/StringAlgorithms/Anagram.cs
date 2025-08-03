b82316c126bcde658a9fd006e751f30f347db819
olha-makarchuk <olamacarchuk@gmail.com>
2025-08-03 20:15:00 +0300

    add anagram chack

---
+++ StringAlgorithms/StringAlgorithms/Anagram.cs
@@ -0,0 +1,18 @@
+﻿namespace StringAlgorithms
+{
+    public class Anagram
+    {
+        public bool Check(string str1, string str2)
+        {
+            if(str1.Length != str2.Length) return false;
+
+            char[] chars1 = str1.ToCharArray();
+            char[] chars2 = str2.ToCharArray();
+
+            Array.Sort(chars1);
+            Array.Sort(chars2);
+
+            return new string(chars1) == new string(chars2);
+        }
+    }
+}
--- StringAlgorithms/StringAlgorithms/Program.cs
+++ StringAlgorithms/StringAlgorithms/Program.cs
@@ -2,3 +2,6 @@
 
 LevenshteinDistance levenshteinDistance = new LevenshteinDistance();
 Console.WriteLine(levenshteinDistance.Find("kitten", "sitting")); // Output: 3
+
+Anagram anagram = new Anagram();
+Console.WriteLine(anagram.Check("listen", "silent")); // Output: True
