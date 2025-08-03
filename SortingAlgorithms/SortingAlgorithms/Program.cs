e4d6d5b2dcc4d000a6d8c7712a72b1a6feeae73b
olha-makarchuk <olamacarchuk@gmail.com>
2025-08-03 18:43:10 +0300

    add timsort

--- SortingAlgorithms/SortingAlgorithms/Program.cs
+++ SortingAlgorithms/SortingAlgorithms/Program.cs
@@ -12,6 +12,15 @@ PrintResult("SelectionSort", new SelectionSort().Sort(Copy(original)));
 PrintResult("ShellSort", new ShellSort().Sort(Copy(original)));
 PrintResult("RadixSort", new RadixSort().Sort(Copy(original)));
 
+int[] arr = {
+    41, 42, 40, 39, 38, 9, 4, 3, 45, 34, 13,14, 29, 31, 33, 32,
+    7, 22, 15, 6, 17, 11, 18, 20, 16, 19, 12, 30,
+    28, 25, 27, 23, 8, 9, 10, 2, 5, 1, 26, 24, 21, 
+    50, 48, 46, 49, 47, 44, 43, 
+};
+
+PrintResult("TimSort", new TimSort().Sort(Copy(arr)));
+
 int[] Copy(int[] arr) => (int[])arr.Clone();
 
 void PrintResult(string name, int[] sorted)
---
+++ SortingAlgorithms/SortingAlgorithms/TimSort.cs
@@ -0,0 +1,64 @@
+﻿namespace SortingAlgorithms
+{
+    //O(n log n)
+    public class TimSort
+    {
+        private readonly MergeSort mergeSort = new();
+
+        public int[] Sort(int[] arr)
+        {
+            if (arr == null || arr.Length <= 1)
+                return arr;
+
+            int runSize = 4;
+
+            // 1. Сортування кожного run за допомогою Insertion Sort
+            for (int i = 0; i < arr.Length; i += runSize)
+            {
+                int right = Math.Min(i + runSize - 1, arr.Length - 1);
+                InsertionSort(arr, i, right);
+            }
+
+            // 2. Поступове злиття відсортованих блоків
+            for (int size = runSize; size < arr.Length; size *= 2)
+            {
+                for (int left = 0; left < arr.Length; left += 2 * size)
+                {
+                    int mid = Math.Min(left + size - 1, arr.Length - 1);
+                    int right = Math.Min(left + 2 * size - 1, arr.Length - 1);
+
+                    if (mid >= right) continue;
+
+                    int[] leftRun = mergeSort.Sort(arr, left, mid);
+                    int[] rightRun = mergeSort.Sort(arr, mid + 1, right);
+                    int[] merged = mergeSort.Sort(leftRun, rightRun);
+
+                    // Копіюємо назад у оригінальний масив
+                    for (int i = 0; i < merged.Length; i++)
+                    {
+                        arr[left + i] = merged[i];
+                    }
+                }
+            }
+
+            return arr;
+        }
+
+        private void InsertionSort(int[] arr, int left, int right)
+        {
+            for (int i = left + 1; i <= right; i++)
+            {
+                int key = arr[i];
+                int j = i - 1;
+
+                while (j >= left && arr[j] > key)
+                {
+                    arr[j + 1] = arr[j];
+                    j--;
+                }
+
+                arr[j + 1] = key;
+            }
+        }
+    }
+}
